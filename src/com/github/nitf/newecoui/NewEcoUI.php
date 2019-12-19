85ce7e3e7107670bd958990db08d40fb2aa033df
github nitf <2tf2003@gmail.com>
2019-12-19 10:35:28 +0900

    intialize

---
+++ src/com/github/nitf/newecoui/NewEcoUI.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace com\github\nitf\newecoui;
+
+use com\github\nitf\newecoui\command\EcoUICommand;
+use pocketmine\plugin\PluginBase;
+
+class NewEcoUI extends PluginBase
+{
+    public function onEnable(): void
+    {
+        // Set up DI.
+        ConfigDI::setUpDI($this->getDataFolder());
+
+        // Register command.
+        $this->getServer()->getCommandMap()
+            ->register("ecoui", new EcoUICommand(
+            "ecoui",
+            "EcoUI Command",
+            "/ecoui"));
+    }
+}
