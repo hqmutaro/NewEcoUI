c1376ff90b5601d0d117708fd0013795b02bdfed
github nitf <2tf2003@gmail.com>
2019-12-30 02:58:51 +0900

    fix: Closureをそのまま実行

--- src/com/github/nitf/newecoui/form/BuildForm.php
+++ src/com/github/nitf/newecoui/form/BuildForm.php
@@ -5,27 +5,22 @@ namespace com\github\nitf\newecoui\form;
 use pocketmine\form\Form;
 use pocketmine\Player;
 
-/**
- * @method onBuild(Player $player)
- */
 class BuildForm
 {
     /** @var Player $player */
     private $player;
 
-    /** @var \Closure $onBuild */
-    private $onBuild;
+    /** @var Form $form */
+    private $form;
 
     public function __construct(Player $player, \Closure $onBuild)
     {
         $this->player = $player;
-        $this->onBuild = $onBuild;
+        $this->form = $onBuild($player);
     }
 
     public function build(): void
     {
-        /** @var Form $form */
-        $form = $this->onBuild($this->player);
-        $this->player->sendForm($form);
+        $this->player->sendForm($this->form);
     }
 }
